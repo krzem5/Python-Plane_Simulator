e8e1dba5eeab63b27a0f5e5eacfad3d332c1679c
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-05-31 10:37:58 +0200

    Push Update 05/31/2020, 10:13:14

--- index.py
+++ index.py
@@ -1,9 +1,7 @@
-(__import__("sys").path.append("D:\\K\\Coding\\projects\\Python-Lwp"),exec("_lwp=__import__(\"lwp\")"),exec(";".join([f"{k}=_lwp.__dict__[\"{k}\"]" for k in dir(_lwp) if k[:2]!="__"])),exec("del _lwp"))
+(__import__("sys").path.append("D:\\K\\Coding\\projects\\Python-Lwp"),exec("_lwp=__import__(\"lwp\")"),exec(";".join([f"{k}=_lwp.__dict__[\"{k}\"]" for k in dir(_lwp) if k[:2]!="__"])),exec("del _lwp"))# from "D:\K\Coding\projects\Python-Lwp\lwp.py" import *
 import time
-
-
-
-VALVE_ANGLE=35
+import json as JSON
+import random
 
 
 
@@ -17,47 +15,39 @@ class Valve(object):
 
 	def extend(self):
 		if (self._st!=1):
-			if (self._st==0):
-				self._m.rotate(VALVE_ANGLE,100,100,0)
-			else:
-				self._m.rotate(VALVE_ANGLE*2,100,100,0)
+			self._m.set_speed(100)
 			self._st=1
 
 
 
 	def retract(self):
 		if (self._st!=-1):
-			if (self._st==0):
-				self._m.rotate(VALVE_ANGLE,-100,100,0)
-			else:
-				self._m.rotate(VALVE_ANGLE*2,-100,100,0)
+			self._m.set_speed(-100)
 			self._st=-1
 
 
 
-	def hold(self):
-		if (self._st!=0):
-			self._m.rotate(VALVE_ANGLE,-100*self._st,100,0)
-			self._st=0
+	def stop(self):
+		self._m.h._send([0x00,0x81,self._m.p,0x11,0x51,0,0])
 
 
 
 class PlaneSimulator(object):
-	def __init__(self,h):
-		self._h=h
-		self.bl=Valve(self,self._h.get_port(0)[0])
-		self.br=Valve(self,self._h.get_port(1)[0])
-		self.fl=Valve(self,self._h.get_port(2)[0])
-		self.fr=Valve(self,self._h.get_port(3)[0])
-		self.bl._m.set_speed(-100)
-		self.br._m.set_speed(-100)
-		self.fl._m.set_speed(-100)
-		self.fr._m.set_speed(-100)
-		time.sleep(0.1)
-		self.bl._m.rotate(VALVE_ANGLE,100,100,0)
-		self.br._m.rotate(VALVE_ANGLE,100,100,0)
-		self.fl._m.rotate(VALVE_ANGLE,100,100,0)
-		self.fr._m.rotate(VALVE_ANGLE,100,100,0)
+	def __init__(self,h1,h2):
+		self._h1=h1
+		self._h2=h2
+		self.bl=Valve(self,self._h1.get_port(0)[0])
+		self.br=Valve(self,self._h1.get_port(1)[0])
+		self.fl=Valve(self,self._h1.get_port(2)[0])
+		self.fr=Valve(self,self._h1.get_port(3)[0])
+		self.s=Valve(self,self._h2.get_port(0)[0])
+		self.bl.retract()
+		self.br.retract()
+		self.fl.retract()
+		self.fr.retract()
+		self.s.retract()
+		time.sleep(0.3)
+
 
 
 	def end(self):
@@ -65,41 +55,50 @@ class PlaneSimulator(object):
 		self.br.retract()
 		self.fl.retract()
 		self.fr.retract()
+		self.s.retract()
+		time.sleep(0.3)
+		self._h1.disconnect()
+		self._h2.disconnect()
+
+
+
+	def run(self,fp,id_=None):
+		with open(fp,"r") as f:
+			dt=JSON.loads(f.read())
+		dt=dt[(id_ if id_!=None else random.randint(0,len(dt)-1))]
+		print(f"Running Flight Path: {dt['name']}")
+		for k in dt["path"]:
+			time.sleep((k["d"] if "d" in k.keys() else 0))
+			for i in range(0,4):
+				if (k["m"][i]!=0):
+					getattr([self.br,self.bl,self.fr,self.fl][i],{-1:"retract",1:"extend"}[k["m"][i]])()
+			print(f"BR: {(self.br._st+1)//2}, BL: {(self.bl._st+1)//2}, FR: {(self.fr._st+1)//2}, FL: {(self.fl._st+1)//2}")
 
 
 
 def main():
-	hl=Hub.find([{0:LargeMotor,1:LargeMotor,2:LargeMotor,3:LargeMotor}],i=2,t=CPlusHub)
-	ps=PlaneSimulator(hl[0][0])
-	input(">>>")
-	ps.fr.extend()
-	ps.fl.extend()
-	time.sleep(2)
-	ps.br.extend()
-	ps.bl.extend()
-	time.sleep(2)
-	ps.fr.retract()
-	ps.fl.retract()
-	time.sleep(0.8)
-	ps.fr.extend()
-	ps.fl.extend()
-	ps.br.retract()
-	ps.bl.retract()
-	time.sleep(1.2)
-	ps.fl.retract()
-	ps.br.extend()
-	time.sleep(2.1)
-	ps.fr.retract()
-	ps.fl.extend()
-	ps.br.retract()
-	ps.bl.extend()
-	time.sleep(2.1)
-	ps.fr.extend()
-	ps.br.extend()
-	time.sleep(2)
-	ps.fr.retract()
-	ps.fl.retract()
-	time.sleep(2)
+	ps=PlaneSimulator(Hub.find([{0:LargeMotor,1:LargeMotor,2:LargeMotor,3:LargeMotor,50:RGBLed}],10,None,lambda h:h.get_port(50)[0].set_color("green"))[0][0],Hub.find([{0:XLMotor,50:RGBLed}],10,None,lambda h:h.get_port(50)[0].set_color("green"))[0][0])
+	ps.s.extend()
+	time.sleep(1)
+	while (True):
+		if (input("Load")!=""):
+			break
+		ps.s.retract()
+		input("Start")
+		ps.run("./flight.json",None)
+		time.sleep(2)
+		ps.br.retract()
+		ps.bl.retract()
+		ps.fr.retract()
+		ps.fl.retract()
+		time.sleep(2.5)
+		ps.br.stop()
+		ps.bl.stop()
+		ps.fr.stop()
+		ps.fl.stop()
+		ps.s.extend()
+		time.sleep(1)
+		ps.s.stop()
 	ps.end()
 
 
